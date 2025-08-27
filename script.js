b91d703d52db21c99a6c047e343382e10950aeeb
Shafin13-bbs <bbshafin13@gmail.com>
2025-08-27 15:18:19 +0600

    call history upgradation coded

--- script.js
+++ script.js
@@ -11,3 +11,40 @@ for(const heart of heartIcons){
        document.getElementById('initial-heart').innerText = addedHearts
     })
 }
+
+
+// Call Button event handler
+
+document.getElementById('all-cards').addEventListener('click',function(e){
+    const callBtn = e.target
+    const serviceName = callBtn.parentNode.parentNode.children[1].children[0].innerText
+    const serviceNumber = parseInt(callBtn.parentNode.parentNode.children[2].children[0].innerText)
+    const coins = parseInt(document.getElementById('coins').innerText)
+    const now = new Date()
+    if(callBtn.className.includes('call-btn')){
+        if(coins >= 20){
+            alert(`Service Name: ${serviceName}\nService Number: ${serviceNumber}\nIt will reduce 20 coins`)
+            const presentCoin = coins - 20
+            document.getElementById('coins').innerText = presentCoin
+        }
+        else{
+            alert("Your don't have enough coins for call\nPlease collect coins")
+            return
+        }
+    }
+
+    const callHistory = document.getElementById('call-history')
+    const updatedHistory = document.createElement('div')
+    updatedHistory.innerHTML = `
+             <div class="flex justify-between items-center my-4 p-3 rounded-xl bg-slate-200">
+                <div>
+                   <h1 class="font-bold text-[17px] w-45">${serviceName}</h1>
+                   <h1 class=" text-xm">${serviceNumber}</h1>
+                </div>
+                <div>
+                   <p>${now.toLocaleTimeString('en-US', { timeZone: 'Asia/Dhaka' })}</p>
+                </div>
+             </div>
+    `
+    callHistory.append(updatedHistory)
+})
