cd5dc0731d0b951a29680282b4d8ac2a1e4b79a8
Shafin13-bbs <bbshafin13@gmail.com>
2025-08-27 18:02:38 +0600

    copy-paste system implemented and call button issue fixed

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-console.log('file working')
-
 
 // Heart Icon Click Handler
 
@@ -12,13 +10,40 @@ for(const heart of heartIcons){
     })
 }
 
+// Copy Button Click Handler
+
+const copyButton = document.getElementsByClassName('copy-button')
+for(const copy of copyButton){
+    copy.addEventListener('click',function(){
+       const initialCopy = parseInt(document.getElementById('initial-copy').innerText)
+       const addedCopy = initialCopy + 1
+       document.getElementById('initial-copy').innerText = addedCopy
+       alert('Number Copied')
+    })
+}
+
+
+// Copy save system Implement
+
+document.getElementById('all-cards').addEventListener('click',function(e){
+    const copied = e.target
+    const serviceNumber = copied.parentNode.parentNode.children[2].children[0].innerText
+    if(copied.className.includes('copy-button')){
+        navigator.clipboard.writeText(serviceNumber)
+    }
+})
+
+
+
+
+
 
 // Call Button event handler
 
 document.getElementById('all-cards').addEventListener('click',function(e){
     const callBtn = e.target
     const serviceName = callBtn.parentNode.parentNode.children[1].children[0].innerText
-    const serviceNumber = parseInt(callBtn.parentNode.parentNode.children[2].children[0].innerText)
+    const serviceNumber = callBtn.parentNode.parentNode.children[2].children[0].innerText
     const coins = parseInt(document.getElementById('coins').innerText)
     const now = new Date()
     if(callBtn.className.includes('call-btn')){
@@ -31,20 +56,25 @@ document.getElementById('all-cards').addEventListener('click',function(e){
             alert("Your don't have enough coins for call\nPlease collect coins")
             return
         }
+        
+        const callHistory = document.getElementById('call-history')
+        const updatedHistory = document.createElement('div')
+        updatedHistory.innerHTML = `
+        <div class="flex justify-between items-center my-4 p-3 rounded-xl bg-slate-200">
+        <div>
+        <h1 class="font-bold text-[17px] w-45">${serviceName}</h1>
+        <h1 class=" text-xm">${serviceNumber}</h1>
+        </div>
+        <div>
+        <p>${now.toLocaleTimeString('en-US', { timeZone: 'Asia/Dhaka' })}</p>
+        </div>
+        </div>
+        `
+        callHistory.append(updatedHistory)
     }
+})
 
+document.getElementById('clear-btn').addEventListener('click',function(){
     const callHistory = document.getElementById('call-history')
-    const updatedHistory = document.createElement('div')
-    updatedHistory.innerHTML = `
-             <div class="flex justify-between items-center my-4 p-3 rounded-xl bg-slate-200">
-                <div>
-                   <h1 class="font-bold text-[17px] w-45">${serviceName}</h1>
-                   <h1 class=" text-xm">${serviceNumber}</h1>
-                </div>
-                <div>
-                   <p>${now.toLocaleTimeString('en-US', { timeZone: 'Asia/Dhaka' })}</p>
-                </div>
-             </div>
-    `
-    callHistory.append(updatedHistory)
+    callHistory.innerHTML = ''
 })
